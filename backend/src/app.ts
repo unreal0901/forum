f4b7dc6813ad86ce8afc8fa0eec3f66cbeb333d5
aditya <rawatas0901@gmail.com>
2023-03-01 14:23:05 +0530

    changed app.ts response on health checker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,7 +39,7 @@ app.get(
   (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({
       status: "success",
-      message: "Welcome to CodevoWeb😂😂👈👈",
+      message: "Testing , Healthy👈👈",
     });
   }
 );
